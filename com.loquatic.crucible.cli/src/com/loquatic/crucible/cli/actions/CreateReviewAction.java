819202961269b7f16a78caebfd8073b7d6e71f6b
Jon Svede <jsvede@yahoo.com>
2013-03-27 13:51:39 -0600

    Removed some functionality from CreateReviewAction and moved it to discreet actions. Now users can addReviewers and addChangesets outside of the create review.

--- com.loquatic.crucible.cli/src/com/loquatic/crucible/cli/actions/CreateReviewAction.java
+++ com.loquatic.crucible.cli/src/com/loquatic/crucible/cli/actions/CreateReviewAction.java
@@ -66,96 +66,7 @@ public class CreateReviewAction extends AbstractAction {
 			success = true ;
 		}
 		
-		String reviewers = commandLine.getOptionValue( CommandLineOption.REVIEWERS.getName() ) ;
 		
-		String changeSet = commandLine.getOptionValue( CommandLineOption.CHANGESET.getName() ) ;
-		
-		String repository = commandLine.getOptionValue( CommandLineOption.REPOSITORY.getName() ) ;
-		
-		if( success ) {
-			success = addReviewersToReview( props, reviewId, reviewers ) ;
-		}
-		
-		if( success ) {
-			success = addChangeSetToReview( props, reviewId, changeSet, repository ) ;
-		}
-		
-		return success ;
-	}
-	
-	public boolean addChangeSetToReview( Properties props, String reviewId, String changeSet, String repoName ) {
-		AddChangesetWrapper changeSetWrapper = createChangesetData( changeSet, repoName ) ;
-		boolean success = false ;
-		
-		StringBuilder url = TargetUrlUtil.createReviewUrl(  props ) ;
-		
-		//TODO: fix this, it's horrible
-		url.append( reviewId ).append("/addChangeset").append( "?" ).append("FEAUTH=").append( getToken() ) ;
-		
-		System.out.println( "URL: " + url.toString() ) ;
-		try {
-			String jsonChangeset = createJsonString( changeSetWrapper ) ;
-			
-			System.out.println( "Changeset request: " + jsonChangeset ) ;
-			
-			ResponseData response  = getHandler().doPost( jsonChangeset, url.toString() ) ;
-			
-			if( response.getHttpStatusCode() >= 200 && response.getHttpStatusCode() < 300 ) {
-				System.out.println( "Successfully added changeset " + changeSet + 
-						                " to review " + reviewId + " in repo " + repoName  ) ;
-				System.out.println( "status: " + response.getStatusLine() + "\n\tresponse data: " + response.getResponseString() ) ;
-				success = true ;
-			} else {
-				System.out.println( "Unable to add changeset(s) " + changeSet + " to review " + reviewId ) ;
-				System.out.println( "System returned: " + response.getStatusLine() ) ;
-			}
-		} catch ( Exception e ) {
-			e.printStackTrace();
-		}
-
-		
-		return success ;
-	}
-	
-	public  AddChangesetWrapper createChangesetData( String revision, String repoName ) {
-		
-		AddChangeset addChangeset = new AddChangeset() ;
-		
-		AddChangesetWrapper wrapper = new AddChangesetWrapper() ;
-
-		String[] changeSets = revision.split(",") ;
-		
-		for ( String aRevision : changeSets ) {
-			ChangesetData changesetData = new ChangesetData();
-			changesetData.setId(aRevision);
-			addChangeset.addChangesetData(changesetData);
-			wrapper.setAddChangeset( addChangeset ) ;
-		}
-		
-		wrapper.setRepository( repoName ) ;
-		
-		return wrapper ;
-	}
-
-	
-	private boolean addReviewersToReview( Properties props, String reviewId, String reviewers ) {
-		
-		boolean success = false ;
-		
-		StringBuilder url = TargetUrlUtil.createReviewUrl(  props ) ;
-		
-		url.append( reviewId ).append( "/reviewers" ).append( "?" ).append("FEAUTH=").append(getToken() ) ;
-		
-		try {
-			ResponseData response = getHandler().doPost( reviewers, url.toString() ) ;
-			if( response.getHttpStatusCode() >= 200 && response.getHttpStatusCode() > 300 ) {
-				System.out.println( "successfully add the reviewers " + reviewers + " to review " + reviewId ) ;
-			}
-			success = true ;
-		} catch ( Exception e ) {
-			e.printStackTrace();
-		}
-
 		return success ;
 	}
 	
@@ -167,7 +78,6 @@ public class CreateReviewAction extends AbstractAction {
 		try {
 			jsonString = createJsonString( reviewData ) ;
 		} catch ( Exception e ) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -206,10 +116,6 @@ public class CreateReviewAction extends AbstractAction {
 	public boolean addOptions(Options options) {
 		myOptions = options ;
 		
-		myOptions.addOption( CommandLineOption.CHANGESET.getName(), 
-				                 true, 
-				                 "Required. The id from the SCM for this " +
-				                 "Crucible project that this review will reference." ) ;
 		myOptions.addOption( CommandLineOption.PROJECT_KEY.getName(), 
 				                 true, 
 				                 "Required. The project key from Crucible " +
@@ -227,9 +133,6 @@ public class CreateReviewAction extends AbstractAction {
 		myOptions.addOption( CommandLineOption.ALLOW_OTHERS_TO_JOIN.getName(), 
 				                 true, 
 				                 "Optional. Defaults to true." ) ;
-		myOptions.addOption( CommandLineOption.REVIEWERS.getName(), true, "Comma " +
-				                 "separated list of Crucible usernames to which " +
-				                 "to assign this review." ) ;
 		myOptions.addOption( CommandLineOption.REPOSITORY.getName(), true, "The " +
 				                 "name of the repository as defined in Crucible." ) ;
 		return true;
@@ -237,23 +140,19 @@ public class CreateReviewAction extends AbstractAction {
 
 	@Override
 	public void printHelp() {
-		HelpFormatter helpFormatter = new HelpFormatter();
-		helpFormatter.printHelp("createReview", myOptions);
+		System.out.println( "--action createReview --projectKey PROJ-ID " +
+			 	            "--reviewName \"My Review of Foo\" --description \"Review of my changes to Foo.\"" +
+				            "--repository myRepo --allowOtherToJoin true" ) ;
+		
 	} 
 	
 	private ReviewData createReviewData( CommandLine commandLine ) {
-		String author = commandLine.getOptionValue( 
-				                 CommandLineOption.USERNAME.getName() ) ;
-		String reviewTitle = commandLine.getOptionValue( 
-				                 CommandLineOption.REVIEW_NAME.getName() ) ;
-		String description = commandLine.getOptionValue( 
-				                 CommandLineOption.DESCRIPTION.getName() ) ;
-		String project = commandLine.getOptionValue( 
-				                 CommandLineOption.PROJECT_KEY.getName() ) ;
-		String allowJoinersStr = commandLine.getOptionValue( 
-				                 CommandLineOption.ALLOW_OTHERS_TO_JOIN.getName() ) ;
+		String author = getUserName( commandLine );
+		String reviewTitle = getReviewName( commandLine ) ;
+		String description = getDescription( commandLine ) ;
+		String project = getProjectKey( commandLine ) ;
+		String allowJoinersStr = getAllowOthersToJoin( commandLine ) ;
 		boolean allowJoiners = Boolean.parseBoolean( allowJoinersStr ) ;
-
 		
 		UserData user = createUserData( author )  ;
 		ReviewData reviewData = new ReviewData() ;
